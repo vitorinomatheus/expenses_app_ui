785240faa031882cd8cf0046062f306259fbc538
vitorinomatheus <matheusvs122@gmail.com>
2024-10-04 19:59:32 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { Roboto } from 'next/font/google'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+})
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +37,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>          
+        </AppRouterCacheProvider>        
       </body>
     </html>
   );
---
+++ src/app/theme.ts
@@ -0,0 +1,23 @@
+'use client';
+import { createTheme, alpha, getContrastRatio } from '@mui/material/styles';
+
+const violetBase = '#5a00b5';
+const violetMain = alpha(violetBase, 0.7);
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'var(--font-roboto)',
+    },
+    cssVariables: true,
+    palette: {
+        mode: 'dark',
+        violet: {
+            main: violetBase,
+            light: alpha(violetBase, 0.5),
+            dark: alpha(violetBase, 0.9),
+            contrastText: getContrastRatio(violetMain, '#fff') > 4.5 ? '#fff' : '#111',
+        }
+    }
+});
+
+export default theme;

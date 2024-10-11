ab50f14eca17f6283c0e438039a89fb064370a33
vitorinomatheus <matheusvs122@gmail.com>
2024-10-11 14:21:06 -0300

    feat: implements navbar icons

--- src/app/components/navBar/appNavBar.tsx
+++ src/app/components/navBar/appNavBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Divider, Drawer, Fab, Stack, Typography } from "@mui/material";
+import { Box, Divider, Drawer, Fab, Icon, Stack, Typography } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import MenuIcon from "@mui/icons-material/Menu"
 import * as React from 'react';
@@ -9,6 +9,7 @@ import VerticalBarProps from "@/app/types/componentsProps/verticalBarProps";
 import { usePageContext } from "@/app/contexts/pageContext";
 import AddIcon from '@mui/icons-material/Add'
 import { APPNAME, MOBILE_MEDIA_QUERY } from "@/app/appConstants";
+import { AccountCircle, Analytics, ArrowDropDown, ArrowDropUp, BarChart, CurrencyExchange, EmojiEmotions, Groups, LocalAtmRounded, Logout, PieChart, PriceChange, ProductionQuantityLimits, QueryStats, ShowChart, TableView } from "@mui/icons-material";
 
 export default function AppNavBar(props: any) {
     let isMobile = useMediaQuery(MOBILE_MEDIA_QUERY);
@@ -82,7 +83,7 @@ function VerticalBar({ onClose }: VerticalBarProps) {
 
     return (
         <div className="flex flex-col items-center p-2 gap-20 bg-transparent" style={{height: '100vh', marginTop: '2rem'}}>
-            <div onClick={() => handleNavigation('/app')} style={{cursor: 'pointer'}} className="select-none">
+            <div onClick={() => handleNavigation('/app')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
                 <Typography variant="h6">{APPNAME}</Typography>
             </div>
             <Stack 
@@ -90,14 +91,14 @@ function VerticalBar({ onClose }: VerticalBarProps) {
                 divider={<Divider orientation="horizontal" flexItem />} 
                 sx={{width: '100%', padding: '1rem'}}
             >
-                <div onClick={() => handleNavigation('/app')} style={{cursor: 'pointer'}} className="select-none">
-                    <Typography>Dashboard inicial</Typography>
+                <div onClick={() => handleNavigation('/app')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                    <BarChart /><Typography>Dashboard inicial</Typography>
                 </div>
-                <div onClick={() => handleNavigation('/app/expenses')} style={{cursor: 'pointer'}} className="select-none">
-                    <Typography>Despesas</Typography>
+                <div onClick={() => handleNavigation('/app/expenses')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                    <LocalAtmRounded /><Typography>Despesas</Typography>
                 </div>
-                <div onClick={() => handleMenuToggle(!openCat, "cat")} style={{cursor: 'pointer'}} className="select-none">
-                    <Typography>Categorias</Typography>
+                <div onClick={() => handleMenuToggle(!openCat, "cat")} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                    <div><TableView />{!openCat ? <ArrowDropDown /> : <ArrowDropUp />}</div><Typography>Categorias</Typography>
                 </div>
                 {openCat ? 
                     <>
@@ -105,28 +106,28 @@ function VerticalBar({ onClose }: VerticalBarProps) {
                             <Stack 
                                 spacing={1}
                                 divider={<Divider orientation="horizontal" flexItem />} >
-                                <div onClick={() => handleNavigation('/app/categories/budgetImpact')} style={{cursor: 'pointer'}} className="select-none">
-                                        <Typography>Impacto</Typography>
+                                <div onClick={() => handleNavigation('/app/categories/budgetImpact')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <PriceChange /><Typography>Impacto</Typography>
                                 </div>
-                                <div onClick={() => handleNavigation('/app/categories/emotional')} style={{cursor: 'pointer'}} className="select-none">
-                                    <Typography>Emoção</Typography>
+                                <div onClick={() => handleNavigation('/app/categories/emotional')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <EmojiEmotions /><Typography>Emoção</Typography>
                                 </div>
-                                <div onClick={() => handleNavigation('/app/categories/expenseFeel')} style={{cursor: 'pointer'}} className="select-none">
-                                    <Typography>Relação c/ despesa</Typography>
+                                <div onClick={() => handleNavigation('/app/categories/expenseFeel')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <ProductionQuantityLimits /><Typography>Relação</Typography>
                                 </div>
-                                <div onClick={() => handleNavigation('/app/categories/expenseType')} style={{cursor: 'pointer'}} className="select-none">
-                                    <Typography>Tipo de despesa</Typography>
+                                <div onClick={() => handleNavigation('/app/categories/expenseType')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <CurrencyExchange /><Typography>Tipo de despesa</Typography>
                                 </div>
-                                <div onClick={() => handleNavigation('/app/categories/social')} style={{cursor: 'pointer'}} className="select-none">
-                                    <Typography>Social</Typography>
+                                <div onClick={() => handleNavigation('/app/categories/social')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <Groups/><Typography>Social</Typography>
                                 </div>
                             </Stack>
                         </div>
                     </>                
                     : null
                 }
-                <div onClick={() => handleMenuToggle(!openDash, "dash")} style={{cursor: 'pointer'}} className="select-none">
-                    <Typography>Análise de despesas</Typography>
+                <div onClick={() => handleMenuToggle(!openDash, "dash")} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                    <div><ShowChart />{!openDash ? <ArrowDropDown /> : <ArrowDropUp />}</div><Typography>Análise</Typography>
                 </div>
                 {openDash ? 
                     <>
@@ -134,22 +135,22 @@ function VerticalBar({ onClose }: VerticalBarProps) {
                             <Stack 
                                 spacing={1}
                                 divider={<Divider orientation="horizontal" flexItem />} >
-                                <div onClick={() => handleNavigation('/login')} style={{cursor: 'pointer'}} className="select-none">
-                                    <Typography>Médias</Typography>
+                                <div onClick={() => handleNavigation('/login')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <PieChart /><Typography>Médias</Typography>
                                 </div>
-                                <div onClick={() => handleNavigation('/login')} style={{cursor: 'pointer'}} className="select-none">
-                                    <Typography>Relações</Typography>
+                                <div onClick={() => handleNavigation('/login')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                                    <QueryStats /><Typography>Relações</Typography>
                                 </div>
                             </Stack>
                         </div>
                     </>                
                     : null
                 }
-                <div onClick={() => handleNavigation('/app/user')} style={{cursor: 'pointer'}} className="select-none">
-                    <Typography>Minha conta</Typography> 
+                <div onClick={() => handleNavigation('/app/user')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                    <AccountCircle /> <Typography>Minha conta</Typography> 
                 </div> 
-                <div onClick={() => handleNavigation('/login')} style={{cursor: 'pointer'}} className="select-none">
-                    <Typography>Logout</Typography> 
+                <div onClick={() => handleNavigation('/login')} style={{cursor: 'pointer', display: 'flex', gap: '1rem'}} className="select-none">
+                    <Logout /> <Typography>Logout</Typography> 
                 </div>                  
             </Stack>
         </div>
